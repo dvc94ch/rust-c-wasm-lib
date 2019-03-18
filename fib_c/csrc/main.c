34d86a17a78574fea6f001cd6ba167f7d7e7cbf6
David Craven <david@craven.ch>
2019-03-18 22:39:39 +0100

    Fix cbindgen emiting unknown names.

--- fib_c/csrc/main.c
+++ fib_c/csrc/main.c
@@ -2,9 +2,9 @@
 #include "bindings.h"
 
 int main() {
-  void* fib = fib_new();
+  FibHandle fib = fib_new();
   for (int i = 0; i < 7; i++) {
-    printf("%d\n", fib_next(fib));
+    printf("%d\n", fib_next(&fib));
   }
   fib_drop(fib);
   return 0;
--- fib_c/src/lib.rs
+++ fib_c/src/lib.rs
@@ -1,21 +1,27 @@
 use fib::Fib;
+use std::ffi::c_void;
+
+#[repr(C)]
+pub struct FibHandle(*mut c_void);
 
 #[no_mangle]
-pub unsafe extern "C" fn fib_new() -> *mut Fib {
-    Box::into_raw(Box::new(Fib::new()))
+pub unsafe extern "C" fn fib_new() -> FibHandle {
+    FibHandle(Box::into_raw(Box::new(Fib::new())) as *mut _)
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn fib_next(fib: *mut Fib) -> u64 {
-    match fib.as_mut() {
+pub unsafe extern "C" fn fib_next(fib: &FibHandle) -> u64 {
+    let ptr = fib.0 as *mut Fib;
+    match ptr.as_mut() {
         Some(fib) => fib.next().unwrap_or(0),
         None => 0
     }
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn fib_drop(fib: *mut Fib) {
-    match fib.as_ref() {
+pub unsafe extern "C" fn fib_drop(fib: FibHandle) {
+    let ptr = fib.0 as *mut Fib;
+    match ptr.as_ref() {
         Some(fib) => drop(fib),
         None => {}
     }
@@ -31,7 +37,7 @@ mod tests {
             let fib = fib_new();
             let mut seq = vec![];
             for _ in 0..7 {
-                seq.push(fib_next(fib));
+                seq.push(fib_next(&fib));
             }
             fib_drop(fib);
             assert_eq!(seq, vec![1, 2, 3, 5, 8, 13, 21]);
